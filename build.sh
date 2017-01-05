cbd9174a23d19ad8e90bb7955b3f8c4946febd4f
jinjor <jinjorweb@gmail.com>
2017-01-06 00:54:38 +0900

    fix regex

---
+++ build.sh
@@ -0,0 +1,1 @@
+zip -r ja-en-separator.zip package
--- package/background.js
+++ package/background.js
@@ -1,7 +1,13 @@
+function replace(s) {
+  return s
+    .replace(/[\x01-\x7E]+/g,' $& ')
+    .replace(/^ ([\x01-\x7E])|([\x01-\x7E]) $/m, '$&');
+}
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId == "separate-ja-en") {
     chrome.tabs.sendMessage(tab.id, { type: "getText" }, function(e) {
-      var result = e.text.replace(/[^\x01-\x7E]+/g,' $& ').replace(/[\s]+/g,' ').trim();
+      var result = e.text.split(/\r\n|\r|\n/).map(replace).join('\n');
       chrome.tabs.sendMessage(tab.id, { type: "setText", text: result });
     });
   }
